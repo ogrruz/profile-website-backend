6ecf2cd7960865ff02ff4fc97aafca432883deb8
ogrruz <garry.logun@gmail.com>
2024-01-16 16:33:34 +0000

    Added CrossOrigin annotation to api

--- backend/demo/src/main/java/com/example/demo/controllers/UserController.java
+++ backend/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping("api/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
